fad31cb15d649cfe1ff8f12e3463acb0ae74945c
Desoppere <59663882+Desoppere@users.noreply.github.com>
2020-12-21 19:38:27 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 function welkeKnop () {
     if (input.buttonIsPressed(Button.AB) || (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
         basic.pause(200)
+        if (input.buttonIsPressed(Button.AB)) {
+            ABknop = 1
+        } else {
+            if (input.buttonIsPressed(Button.A)) {
+                Aknop = 1
+            }
+            Bknop = 1
+        }
     } else {
-        welkeKnop()
+    	
     }
 }
 function opstartscherm () {
@@ -59,10 +67,14 @@ function opstartscherm () {
         `)
     basic.pause(200)
 }
-opstartscherm()
-let Aknop = 0
-let Bknop = 0
 let ABknop = 0
+let Bknop = 0
+let Aknop = 0
+opstartscherm()
+Aknop = 0
+Bknop = 0
+ABknop = 0
 basic.forever(function () {
-	
+    welkeKnop()
+    basic.showNumber(ABknop)
 })
